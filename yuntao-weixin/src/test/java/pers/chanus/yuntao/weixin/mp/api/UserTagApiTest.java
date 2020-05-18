aeeb091388f2614f8fba0fc38c20ff1e4921bd36
Chanus <chanus1990@163.com>
2020-05-18 23:15:30 +0800

    完成微信公众号用户标签管理的接口封装

--- yuntao-weixin/src/test/java/pers/chanus/yuntao/weixin/mp/api/UserTagApiTest.java
+++ yuntao-weixin/src/test/java/pers/chanus/yuntao/weixin/mp/api/UserTagApiTest.java
@@ -9,6 +9,13 @@
  */
 package pers.chanus.yuntao.weixin.mp.api;
 
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+import pers.chanus.yuntao.weixin.mp.api.bean.ApiConfig;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * UserTagApi 测试
  *
@@ -17,4 +24,72 @@ package pers.chanus.yuntao.weixin.mp.api;
  * @since 0.1.9
  */
 public class UserTagApiTest {
+    private String appId = "wxe1b1996ffb222290";
+    private String appSecret = "3eff2d48cab4356dd096228528a1537c";
+
+    @Test
+    public void createTest() {
+        AccessTokenApi.apiConfig = new ApiConfig(appId, appSecret);
+        JSONObject jsonObject = UserTagApi.create("福建");
+        System.out.println(jsonObject.toJSONString());
+    }
+
+    @Test
+    public void getTest() {
+        AccessTokenApi.apiConfig = new ApiConfig(appId, appSecret);
+        JSONObject jsonObject = UserTagApi.get();
+        System.out.println(jsonObject.toJSONString());
+    }
+
+    @Test
+    public void updateTest() {
+        AccessTokenApi.apiConfig = new ApiConfig(appId, appSecret);
+        JSONObject jsonObject = UserTagApi.update(101, "安徽");
+        System.out.println(jsonObject.toJSONString());
+    }
+
+    @Test
+    public void deleteTest() {
+        AccessTokenApi.apiConfig = new ApiConfig(appId, appSecret);
+        JSONObject jsonObject = UserTagApi.delete(100);
+        System.out.println(jsonObject.toJSONString());
+    }
+
+    @Test
+    public void getTagUsersTest() {
+        AccessTokenApi.apiConfig = new ApiConfig(appId, appSecret);
+        JSONObject jsonObject = UserTagApi.getTagUsers(100, null);
+        System.out.println(jsonObject.toJSONString());
+    }
+
+    @Test
+    public void batchTaggingTest() {
+        AccessTokenApi.apiConfig = new ApiConfig(appId, appSecret);
+        List<String> openIdList = new ArrayList<>();
+        openIdList.add("o-mAK55lxMjG-3Kd5wRtFtS__4rA");
+        openIdList.add("o-mAK5_H-VujM2sT7SYs1pyJEEio");
+        openIdList.add("o-mAK56WJbFNVxrptEbo80apufVc");
+        openIdList.add("o-mAK55qfZGGZLh-BV4AG78sXgtQ");
+        JSONObject jsonObject = UserTagApi.batchTagging(100, openIdList);
+        System.out.println(jsonObject.toJSONString());
+    }
+
+    @Test
+    public void batchUntaggingTest() {
+        AccessTokenApi.apiConfig = new ApiConfig(appId, appSecret);
+        List<String> openIdList = new ArrayList<>();
+        openIdList.add("o-mAK55lxMjG-3Kd5wRtFtS__4rA");
+        openIdList.add("o-mAK5_H-VujM2sT7SYs1pyJEEio");
+        openIdList.add("o-mAK56WJbFNVxrptEbo80apufVc");
+        openIdList.add("o-mAK55qfZGGZLh-BV4AG78sXgtQ");
+        JSONObject jsonObject = UserTagApi.batchUntagging(100, openIdList);
+        System.out.println(jsonObject.toJSONString());
+    }
+
+    @Test
+    public void getUserTagsTest() {
+        AccessTokenApi.apiConfig = new ApiConfig(appId, appSecret);
+        JSONObject jsonObject = UserTagApi.getUserTags("o-mAK56WJbFNVxrptEbo80apufVc");
+        System.out.println(jsonObject.toJSONString());
+    }
 }
