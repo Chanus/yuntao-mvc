950d29a7d4a97c318ea831d11ffe0c5488b7cc50
ChanusYun <chanus1990@gmail.com>
2020-12-02 09:31:18 +0800

    v1.1.3 1. layui 升级到 2.5.7 版本； 2. logback 日志设置压缩保存，并单独保存错误日志。

--- yuntao-mvc-framework/src/main/java/com/chanus/yuntao/mvc/framework/ConfigServlet.java
+++ yuntao-mvc-framework/src/main/java/com/chanus/yuntao/mvc/framework/ConfigServlet.java
@@ -49,7 +49,7 @@ public class ConfigServlet extends HttpServlet {
                 " \\____   |  |  /   |  \\  |  / __ \\(  <_> ) /    Y    \\     /  \\     \\____\n" +
                 " / ______|____/|___|  /__| (____  /\\____/  \\____|__  /\\___/    \\______  /\n" +
                 " \\/                 \\/          \\/                 \\/                 \\/ \n" +
-                "                                                              v1.0.0\n";
+                "                                                              v1.1.3\n";
         System.out.println(logo);
 
         ServletContext servletContext = getServletContext();
--- yuntao-mvc-manager/yuntao-mvc-manager-web/src/test/java/com/chanus/yuntao/mvc/manager/test/LicenseUtilsTest.java
+++ yuntao-mvc-manager/yuntao-mvc-manager-web/src/test/java/com/chanus/yuntao/mvc/manager/test/LicenseUtilsTest.java
@@ -32,11 +32,11 @@ public class LicenseUtilsTest {
         String privateKey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAImZ6XJVDQspW55LunRBrlMrozvh5EXxN7Zjlr03NLenFU2dAJ5moGdGrPlLhMHUDPAqrEh93pUJeLlLIiYErY+8FwDu6Tim/wASgcCihe2mauUqrEiXHmuTjIiEggY7WteT8anDGuuZFvBNhrOMSMIg8B0fs8mdf+jDv5bAn0rLAgMBAAECgYAos17q0591Ermn2+x94MOolDrHRqzXx17cUx7OPxsXy5MwjuUU9XQyuCQH5T+SeZ+OFoE5PAnveYq8/C1Qo4eWkddu/+uN5oNlvoRI5yXR17utMOEEgv5ek2wUw2mOasbARTOHjXDqNGXGjJDH/tFipWmpdGTbXP5SRyp+Wm4/2QJBAM6hxJXTsIViI2pQE35DdhMv4T2ha9M6p0lEZ8ZMgV93Uuffq5jg45KqkfVXLhyTi0D5ELjId/R9kN/O2wQasH8CQQCqegS4Ilt0R2bmQyR3VF/qbGg4BhpfFtcjVAzwyL4hHOKHR+B9wyDf9ElKGoQiXjTexb0ZRsmc8WV1tlDjDv+1AkBm33rz7PSRM47WOw+5aPS8ELZe7KK2AxmODJRt0WEHmmyYLG9OKYijWHBaaRD4ASzvKBD/Li8qdelOw7mejXsZAkEAlZoOmihMaXZfbqJtVerNYUNREhkI+3lnwrD3yjXf498dS+pfrv2V0/LkfgtW+XICo5yTnaLpOrj8eJhN03ROIQJAQ0h+fP9/lSIwha4q4z/miMFB0zYsbBxiOiUctkIKO7uHQNmJfnvVUbApiX/xJ5n2yjTf2OGph1VbAZQ6/cv1pw==";
         String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCJmelyVQ0LKVueS7p0Qa5TK6M74eRF8Te2Y5a9NzS3pxVNnQCeZqBnRqz5S4TB1AzwKqxIfd6VCXi5SyImBK2PvBcA7uk4pv8AEoHAooXtpmrlKqxIlx5rk4yIhIIGO1rXk/GpwxrrmRbwTYazjEjCIPAdH7PJnX/ow7+WwJ9KywIDAQAB";
 
-        // String path = "F:/license.lic";// windows
-        String path = "/Users/Chanus/Documents/license.lic";// mac
+        String path = "F:/license.lic";// windows
+        // String path = "/Users/Chanus/Documents/license.lic";// mac
 
         String name = "yundao-mvc-manager";
-        String version = "1.0.0";
+        String version = "1.1.3";
         // Set<String> macAddress = SystemUtils.getHostMac();
         String macAddress = null;
         String limit = "0";
